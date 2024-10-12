fa0af997118945d075e1be655d42ab9ba2f307c4
Amarjeet Kumar <masteramarjeet@users.noreply.github.com>
2024-10-12 23:12:16 +0530

    add tailwind

--- packages/www/astro.config.mjs
+++ packages/www/astro.config.mjs
@@ -2,9 +2,12 @@ import { defineConfig } from 'astro/config'
 
 import cloudflare from '@astrojs/cloudflare'
 
+import tailwind from '@astrojs/tailwind'
+
 // https://astro.build/config
 export default defineConfig({
   output: 'hybrid',
   adapter: cloudflare(),
   server: { open: true },
+  integrations: [tailwind()],
 })
