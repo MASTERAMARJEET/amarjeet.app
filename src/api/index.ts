5c69d2522900ca2da72561934fddf4927d7e5bc5
Amarjeet Kumar <masteramarjeet@users.noreply.github.com>
2024-11-01 12:54:54 +0530

    feat: :sparkles: add hono route

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,9 +1,19 @@
 import { Hono } from 'hono';
+import { prettyJSON } from 'hono/pretty-json';
 
 const app = new Hono();
+
+app.use(prettyJSON());
 
 app.get('/', (c) => {
   return c.text('Hello from amarjeet!');
 });
 
+app.get('/meta', (c) => {
+  return c.json({
+    name: 'Amarjeet App',
+  });
+});
+
+export type AppType = typeof app;
 export default app;
